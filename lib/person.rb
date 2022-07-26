ba69b538d5ccfe8b3595c3d19e31c66193a1a2c4
Terence Holmes <piece4ulmind@gmail.com>
2022-07-25 21:01:46 -0400

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,89 @@
 # your code goes here
+class Person
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+  
+    def initialize(name)
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygiene = 8
+    end
+  
+    def happiness=(num)
+        @happiness = num
+       if @happiness > 10
+            @happiness = 10 
+       end
+       if @happiness < 0
+            @happiness = 0
+       end
+    end
+
+    def hygiene=(num)
+        @hygiene = num
+        if @hygiene > 10
+            @hygiene = 10
+        end
+        if @hygiene < 0
+            @hygiene = 0 
+        end
+    end
+
+    def clean?
+        if hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+         if topic == "politics" 
+            person.happiness -= 2
+            self.happiness -= 2
+           "blah blah partisan blah lobbyist"
+         elsif 
+            topic == "weather"
+            person.happiness += 1
+            self.happiness += 1
+            "blah blah sun blah rain"
+        else
+            'blah blah blah blah blah'
+        end       
+    end
+
+
+             
+end
